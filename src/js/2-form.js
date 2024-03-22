bdce07603ece285009de8874b95201fe3ed82f1d
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-22 22:13:18 +0200

    task 2 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,39 @@
 const form = document.querySelector(".feedback-form")
+const STORAGE_KEY = "data"
 
-form.addEventListener("input", ((event) => {
-    // let email
-    // let comment
-    if (event.target.className === "email-input") {
-        // email = event.target.value
-        localStorage.setItem("email", event.target.value);
-    } else {
-        // comment = event.target.value
-        localStorage.setItem("comment", event.target.value);
+let tank = {
+        email: "",
+        message: ""
     }
+form.addEventListener("input", ((event) => {
+    if (event.target.className === "email-input") {
+        tank.email = event.target.value.trim()
+    } else {
+        tank.message = event.target.value.trim()
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tank))
 }))
 
+
+const emailInput = document.querySelector(".email-input")
+const textArea = document.querySelector(".textarea")
+
+const jsn = localStorage.getItem(("data"))
+if (jsn) {
+emailInput.value = JSON.parse(jsn).email
+textArea.value = JSON.parse(jsn).message
+} else {
+    emailInput.value = ""
+    textArea.value = ""
+}
+
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-	console.log(event.target.elements.message.value);
-    localStorage.removeItem("email");
-    localStorage.removeItem("comment");
+    event.preventDefault();
+    if (emailInput.value === '' || textArea.value === '') {
+        alert('All form fields must be filled in')
+        return
+    }
+    console.log(JSON.parse(localStorage.getItem("data")));
+    localStorage.removeItem("data");
   form.reset();
 });
